3acdd05e11f3d20e305857397adbf6b26a2ec3c4
Артамонов Алексей Викторович <94192633+firstConsole@users.noreply.github.com>
2023-06-10 19:46:21 +0300

    Initial Commit

---
+++ Let Party/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Let Party
+//
+//  Created by Алексей Артамонов on 10.06.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Let Party/Let_PartyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Let_PartyApp.swift
+//  Let Party
+//
+//  Created by Алексей Артамонов on 10.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct Let_PartyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
